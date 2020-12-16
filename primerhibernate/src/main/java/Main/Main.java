6ce558907df25c2288b76e6a4965bd3ff0d42359
alvar <alvar@DESKTOP-K8EHG61>
2020-12-16 09:04:51 +0100

    Ejercicio 2 hibernate

--- primerhibernate/src/main/java/Main/Main.java
+++ primerhibernate/src/main/java/Main/Main.java
@@ -1,5 +1,7 @@
 package Main;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
@@ -11,6 +13,8 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 public class Main {
+	
+	private static final String RUTA_FICHERO = "Alumnado_nuevo.txt";
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -27,20 +31,43 @@ public class Main {
     	//prueba1A1 clase2= new prueba1A1("esto no es nada");
     	//clase.setPrueba(clase2);
     	//clase2.setPrueba(clase);
-    	profesor profesor=new profesor(7, "Sara", "Barrrera", "Salas");
-    	Set<alumno> alumnos=new HashSet();
-    	alumnos.add(new alumno(1,"pepe",profesor));
-    	alumnos.add(new alumno(2,"juan",profesor));
-    	alumnos.add(new alumno(3,"clara",profesor));
+    	//profesor profesor=new profesor(7, "Sara", "Barrrera", "Salas");
+    	//Set<alumno> alumnos=new HashSet();
+    	//alumnos.add(new alumno(1,"pepe",profesor));
+    	//alumnos.add(new alumno(2,"juan",profesor));
+    	//alumnos.add(new alumno(3,"clara",profesor));
     	
-    	profesor.setAlumnos(alumnos);
+    	//profesor.setAlumnos(alumnos);
     	
-    	sesion.save(profesor);
+    	//sesion.save(profesor);
     	//guardar alumno
     	//sesion.save(clase2);
     	//sesion.save(clase);
     	
-    	sesion.getTransaction().commit();
+    	BufferedReader lector = null;
+		String linea = null;
+		ArrayList<SUPERusuario> superusuarios = new ArrayList();
+
+		// Lectura de fichero e inserciones en tabla
+		lector = new BufferedReader(new FileReader(RUTA_FICHERO));
+		while ((linea = lector.readLine()) != null) {
+			String[] campos = linea.split(",");
+			String apellidos = campos[0];
+			String nombre = campos[1];
+			String user = getUser(nombre, apellidos);
+
+			// Añadir superusuarios a la lista
+			superusuarios.add(new SUPERusuario(nombre, apellidos, user));
+		}
+		// Cerrar lector
+		lector.close();
+		
+		for (SUPERusuario superusuario : superusuarios) {
+			sesion.save(superusuario);
+		}
+
+		sesion.getTransaction().commit();
+    	
     	}catch(Exception e){
     		e.printStackTrace();
     	} finally {
@@ -48,6 +75,12 @@ public class Main {
 	    	sf.close();
     	}
     	
+    		
+    	
+	}
+	
+	private static String getUser(String nombre, String apellidos) {
+		return "2DAM" + nombre.charAt(1) + apellidos.charAt(0);
 	}
 
 }
---
+++ primerhibernate/src/main/java/Main/SUPERusuario.java
@@ -0,0 +1,67 @@
+package Main;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+
+@Entity
+public class SUPERusuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idUser;
+	@Column
+	private String nombre;
+	@Column
+	private String apellidos;
+	@Column
+	private String user;
+	
+public SUPERusuario() {}
+	
+	
+
+	public SUPERusuario(String nombre, String apellidos, String user) {
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.user = user;
+	}
+
+
+	public int getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(int idUser) {
+		this.idUser = idUser;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+}
